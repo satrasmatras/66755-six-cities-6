32aeeb1f4036c9bd1834c5b264de6f0bf0fe047a
Denis Gorshkov <satrasmatrassatras@gmail.com>
2021-02-05 21:23:20 +0300

    Добавляет index.js

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById(`root`)
+    <App />,
+    document.getElementById(`root`)
 );
