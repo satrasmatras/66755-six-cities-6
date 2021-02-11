4571358c9a711bfc5ac3ce17bd32f203f3c47ecb
Denis Gorshkov <satrasmatrassatras@gmail.com>
2021-02-11 14:46:26 +0300

    Вставляет минимальную длину отзыва в разметку

--- src/components/createCommentForm/createCommentForm.tsx
+++ src/components/createCommentForm/createCommentForm.tsx
@@ -90,7 +90,7 @@ const CreateCommentForm = (): ReactElement => {
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and
-          describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          describe your stay with at least <b className="reviews__text-amount">{MIN_REVIEW_LENGTH} characters</b>.
         </p>
         <button
           className="reviews__submit form__submit button"
