423463d58313d12c21d163cdf4e5291fb75007b1
Denis Gorshkov <satrasmatrassatras@gmail.com>
2021-02-11 14:07:49 +0300

    Изменяет получение названия месяца

--- src/utils.ts
+++ src/utils.ts
@@ -10,22 +10,23 @@ export const formatDateToMachine = (date: Date): string => {
   return `${year}-${month}-${day}`;
 };
 
+const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
 const getMonthNameByIndex = (index: number): string => {
-  switch (index) {
-    case 0: return `January`;
-    case 1: return `February`;
-    case 2: return `March`;
-    case 3: return `April`;
-    case 4: return `May`;
-    case 5: return `June`;
-    case 6: return `July`;
-    case 7: return `August`;
-    case 8: return `September`;
-    case 9: return `October`;
-    case 10: return `November`;
-    case 11: return `December`;
-    default: return `Unknown`;
-  }
+  return MONTH_NAMES[index];
 };
 
 export const formatDateToHuman = (date: Date): string => {
